f9de1b02aa23ab059aee8427ce55c928ce1206ad
Michel Domingos <michel.silva@penseavanti.com.br>
2022-09-03 17:53:34 -0300

    feat: creating cart and details page

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Cart from './pages/cart'
 import Home from './pages/home'
+import Details from './pages/pdp'
 
 export const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/pdp' element={<Details />} />
       </Routes>
     </Router>
   )
