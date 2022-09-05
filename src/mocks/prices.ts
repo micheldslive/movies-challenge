e1539a414bcb4314514351eb2815477ce2c0ac67
Michel Domingos <michel.silva@penseavanti.com.br>
2022-09-04 23:26:29 -0300

    feat: creating methods and mock

---
+++ src/mocks/prices.ts
@@ -0,0 +1,16 @@
+interface IMapedPrices {
+  [key: number]: number
+}
+
+export const prices: IMapedPrices = {
+  1: 19.99,
+  2: 29.99,
+  3: 39.99,
+  4: 49.99,
+  5: 59.99,
+  6: 69.99,
+  7: 79.99,
+  8: 89.99,
+  9: 99.99,
+  10: 109.99,
+}
