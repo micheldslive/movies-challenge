0d680ea4e3409887d1d5fd2ac3245702f5ce7b9b
Michel Domingos <michel.silva@penseavanti.com.br>
2022-09-05 09:10:01 -0300

    chore: changing wishlist to wishlistIcon

--- src/components/wishlistIcon/wishlist.test.tsx
+++ src/components/wishlistIcon/wishlist.test.tsx
@@ -1,9 +1,9 @@
 import { getRenderer } from '@/core/tests/helpers'
-import Wishlist from './index'
+import WishlistIcon from './index'
 
-describe('<Wishlist />', () => {
-  it('should have a link and svg element on Wishlist', () => {
-    const { getByLabelText } = getRenderer(<Wishlist />)
+describe('<WishlistIcon />', () => {
+  it('should have a link and svg element on WishlistIcon', () => {
+    const { getByLabelText } = getRenderer(<WishlistIcon />)
 
     const link = getByLabelText('link')
     const svg = getByLabelText('svg')
