d0547da662db1ea3bf61d2e5dc39048e4b3f0190
Michel Domingos <michel.silva@penseavanti.com.br>
2022-09-05 09:11:08 -0300

    fix: adjustment in pricing methods

--- src/core/utils/price.ts
+++ src/core/utils/price.ts
@@ -1,14 +1,14 @@
 import { prices } from '@/mocks/prices'
 
 export const getPrice = (rating: number) => {
-  const roundRating = Math.round(rating < 11 ? rating : 11)
+  const roundRating = Math.round(rating < 11 ? rating : 10)
   const renderPrice = prices[roundRating]
 
   return renderPrice
 }
 
 export const formatPrice = (price: number) => {
-  const newPrice = price.toLocaleString('pt-BR', {
+  const newPrice = price?.toLocaleString('pt-BR', {
     minimumFractionDigits: 2,
     style: 'currency',
     currency: 'BRL',
