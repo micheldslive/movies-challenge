dc41b58a29ad845d7bd97d35408e24d02b66e203
Michel Domingos <michel.silva@penseavanti.com.br>
2022-09-06 08:55:57 -0300

    chore: adding toastify container

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/header'
 import { Routers } from './routers'
 import './styles/index.scss'
+import 'react-toastify/dist/ReactToastify.min.css'
+import { Toast } from './components/toast'
 
 function App() {
   return (
     <Router>
       <Header />
       <Routers />
+      <Toast />
     </Router>
   )
 }
